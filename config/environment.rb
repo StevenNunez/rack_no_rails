06e2cbcd30f6865f3136b988005784abf3dbbca7
Steven Nunez <steven.nunez@gmail.com>
2015-08-23 21:06:08 -0400

    Add Model and load all Models and Controllers

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,11 @@ require 'bundler'
 Bundler.require(:default)
 
 ActiveRecord::Base.establish_connection(
-  adapter: 'sqlite3', 
+  adapter: 'sqlite3',
   database: 'potd_development.db'
 )
+
+$: << '.'
+
+Dir['app/controllers/*.rb'].each {|f| require f}
+Dir['app/models/*.rb'].each {|f| require f}
